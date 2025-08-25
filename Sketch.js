8c38bed1546c6e23f9c82b950acd6e5bc39e2f2c
Angelina Caviedes N <angelina.caviedes@ug.uchile.cl>
2025-08-25 18:09:28 -0400

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,28 +1,26 @@
+var valor = document.getElementById("pecinco").offsetWidth;
 
-            var valor = document.getElementById("pecinco").offsetWidth;
+window.addEventListener("resize", function () {
+    valor = document.getElementById("pecinco").offsetWidth;
+});
 
-            window.addEventListener("resize", function () {
-                valor = document.getElementById("pecinco").offsetWidth;
-            });
+function setup() {
+    createCanvas(valor, valor, WEBGL).parent("#pecinco");
+    select("#nombre").html("marti U・ᴥ・U");
+    select("#aprendizaje").html("a programar y explorar nuevas formas de expresión visual para proyectos digitales");
+    noStroke();
+}
+function draw() {
+    background(230, 230, 250);
+    orbitControl();
+    pointLight(255, 255, 255, valor / 3, valor / 2, 50);
+    specularMaterial(255);
+    shininess(50);
+    metalness(1);
+    fill(139, 7, 139);
+    ellipsoid(30);
+}
 
-            function setup() {
-                createCanvas(valor, valor, WEBGL).parent("#pecinco");
-                select("#nombre").html("Angelina Caviedes");
-                select("#aprendizaje").html("Me gustaria aprender más sobre herramientas computacionales, como illustrator, que me ayuden a potenciar mis proyectos");
-            }
-            noStroke();
-            function draw() {
-                background(255, 216, 223);
-                orbitControl();
-                pointLight (255, 255, 255, valor/3, valor/2, 50);
-                specularMaterial(255);
-                shininess(50);
-                metalness(1);
-                fill(70, 120, 255);
-                sphere(valor/4, valor/4);
-            }
-
-            function windowResized() {
-                resizeCanvas(valor, valor);
-            }
-     
+function windowResized() {
+    resizeCanvas(valor, valor);
+}
